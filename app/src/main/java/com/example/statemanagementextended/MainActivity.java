ad4b48a89fbfb41faf0b34d3978b7c647f122f52
Infini <106250551+Krygsm@users.noreply.github.com>
2024-10-10 12:52:55 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/statemanagementextended/MainActivity.java
+++ app/src/main/java/com/example/statemanagementextended/MainActivity.java
@@ -70,19 +70,21 @@ public class MainActivity extends AppCompatActivity
 
         editText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) 
+            {
 
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) 
+            {
 
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {
+            public void afterTextChanged(Editable editable) 
+            {
                 text = editText.getText().toString();
-                updateEditText();
             }
         });
 
