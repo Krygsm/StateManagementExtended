c48c958757c202b3314b6a9c2ae48e92bb72a96a
Infini <106250551+Krygsm@users.noreply.github.com>
2024-10-10 11:20:11 +0200

    zadanie na 5 poprawka

--- app/src/main/java/com/example/statemanagementextended/MainActivity.java
+++ app/src/main/java/com/example/statemanagementextended/MainActivity.java
@@ -68,6 +68,24 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                text = editText.getText().toString();
+                updateEditText();
+            }
+        });
+
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
         {
             @Override
